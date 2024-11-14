4773839ae87e9d7b45557cfa97eb3418b6a993b8
mason kamolpechara <mason.kamolpechara@gmail.com>
2024-11-14 17:12:41 +0900

    basic functions done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,28 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+    @movie = Movie.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params_strong)
+    if (@list.save)
+      redirect_to list_path(@list)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def params_strong
+    params.require(:list).permit(:name)
+  end
+end
